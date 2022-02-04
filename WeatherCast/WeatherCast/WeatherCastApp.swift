d31391641b0c7ec14f6d71221dfc5d95e8598a29
Ravi Kiran HR <ravi.kiran@philips.com>
2022-02-04 22:07:22 +0530

    Initial Commit

---
+++ WeatherCast/WeatherCast/WeatherCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherCastApp.swift
+//  WeatherCast
+//
+//  Created by Ravi Kiran HR on 04/02/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
